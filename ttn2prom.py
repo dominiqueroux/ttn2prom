3ebd3ca4208f1941624bc7be85a06029236e2fd3
Dominique Roux <dominique.roux@protonmail.com>
2021-07-05 15:54:36 +0200

    Added two additional metrics: - battery level - timestamp of last arrival of data

--- ttn2prom.py
+++ ttn2prom.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import time
 
 from klein import Klein
 from prometheus_client import Gauge
@@ -17,15 +18,19 @@ def decode_payload(raw):
     p = base64.b64decode(raw)
     raw_t = int.from_bytes(p[1:3], byteorder='little', signed=False)
     raw_h = p[3]
+    raw_b = p[8]
     t = (175.72 * raw_t) / (2**16) - 46.85
     h = (125    * raw_h) / (2**8)  - 6
-    return (t, h)
+    b = (raw_b + 150) * 0.01
+    return (t, h, b)
 
 
 app = Klein()
 
 g_temperature = Gauge('lorasensor_temperature_celsius', 'Temperature from the sensor',       ['device'])
 g_humidity    = Gauge('lorasensor_humidity_percent',    'Relative humidity from the sensor', ['device'])
+g_battery     = Gauge('lorasensor_battery_volt',        'Battery level from the senser',     ['device'])
+g_timestamp   = Gauge('lorasensor_data_timestamp',      'Timestamp of last incoming data',   ['device'])
 
 @app.route('/', methods=['POST'])
 def save_item(request):
@@ -35,12 +40,14 @@ def save_item(request):
     if not is_interesting(data): return json.dumps({'success': True})
 
     dev = data["dev_id"]
-    t, h = decode_payload(data["payload_raw"])
+    t, h, b = decode_payload(data["payload_raw"])
 
-    print(" *** device {} sent payload: {}, {}".format(dev, t, h))
+    print(" *** device {} sent payload: {}, {}, {}".format(dev, t, h, b))
 
     g_temperature.labels(device=dev).set(t)
     g_humidity.labels(device=dev).set(h)
+    g_battery.labels(device=dev).set(b)
+    g_timestamp.labels(device=dev).set(time.time())
 
     return json.dumps({'success': True})
 
